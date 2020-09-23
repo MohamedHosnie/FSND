e28095dbdd7ff4863f30910d200c01afdd795e8d
MohamedHosnie <mohamed.hosnie1@gmail.com>
2020-09-23 02:18:36 +0200

    Minor fix in the display of dates

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -74,12 +74,11 @@ class Show(db.Model):
 # Filters.
 #----------------------------------------------------------------------------#
 def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
   if format == 'full':
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
       format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format)
+  return babel.dates.format_datetime(value, format)
 app.jinja_env.filters['datetime'] = format_datetime
 
 
